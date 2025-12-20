b563b20261cf63138aba7c652bcf586222399c18
ahmad hosseini <moze.mehraban@gmail.com>
2025-12-20 16:49:05 +0330

    trying  to make the image

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "data structure tools",
   description: "Don't just read code—see it in action. The ultimate interactive playground to visualize Data Structures and learn algorithms faster.",
   openGraph: {
-    images: [  '/og-image.png'  ],
+    images: [  'public/og-image.png'  ],
     description: "Don't just read code—see it in action. The ultimate interactive playground to visualize Data Structures and learn algorithms faster."  
   }
 };
